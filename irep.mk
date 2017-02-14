8005f2bb3cc87bef528e19e4d4dd264bbcb3d4ab
Lee Busby <busby1@llnl.gov>
2017-02-14 14:29:01 -0800

    Changes to support null termination of strings in IREP.

--- irep.mk
+++ irep.mk
@@ -13,11 +13,44 @@ ch_comma := ,
 ch_semi  := ;
 ccE = gcc -E
 
-# Override these variables in the client makefile as needed.
-CC = gcc
-FC = gfortran
-CFLG =
-FFLG = -cpp -ffree-form -ffree-line-length-0
+# Gnu compilers (gcc, g++, gfortran) require version 4.7.4 or greater.
+gnu.cc = gcc
+gnu.fc = gfortran
+gnu.cflg =
+gnu.fflg = -cpp -fbackslash -ffree-form -ffree-line-length-0
+gnu.lflags = -lm -lgfortran -ldl
+gnu.link = $(gnu.cc)
+
+# Intel-16 is known to work.
+intel.cc = icc
+intel.fc = ifort
+intel.cflg =
+intel.fflg = -fpp -assume bscc -free
+intel.lflags = -limf -lirc -lifcore
+intel.link = $(intel.cc)
+
+# Pgi-16 is known to work.
+pgi.cc = pgcc -pgf90libs
+pgi.fc = pgfortran
+pgi.cflg =
+pgi.fflg = -Mfree -Mrecursive -Mpreprocess -Mnobackslash
+pgi.lflags = -ldl
+pgi.link = $(pgi.cc)
+
+ibm.cc = bgxlc_r
+ibm.fc = bgxlf_r -qfree=f90 -qsuffix=cpp=f:f=f -qlanglvl=extended
+ibm.cflg =
+ibm.fflg =
+#ibm.lflags = -L/opt/ibmcmp/xlf/bg/14.1/lib -lxlf90 -lm -lc
+ibm.lflags =
+ibm.link = $(ibm.fc)
+
+CC = $($(compiler).cc)
+FC = $($(compiler).fc)
+CFLG = $($(compiler).cflg)
+FFLG = $($(compiler).fflg)
+LINK = $($(compiler).link)
+
 fppflags_extra = 
 Lua_include = $(HOME)/$(SYS_TYPE)/lua515/include
 FPPFLAGS = -DLANG_FORTRAN -I$(Lua_include) -I$(ir.dir) $(fppflags_extra)
@@ -50,19 +83,19 @@ irep.o : irep.c ir_special.h
 $(ir.wfs) : ir_std.mod
 wkt_%.f : wkt_%.h ir_macros.h ir_start.h ir_end.h
 	$(ccE) -DIR_WKT_NAME=wkt_$* $(FPPFLAGS) $< | \
-	  sed -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
+	  sed -e 's:+/+/://:' -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
 
 ir_std.f : ir_std.h
 	$(ccE) -DIR_WKT_NAME=ir_std $(FPPFLAGS) $< | \
-	  sed -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
+	  sed -e 's:+/+/://:' -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
 
 ir_readnml.o ir_readnml.mod : ir_std.mod $(ir.wmod)
 ir.wuse = $(addprefix use ,$(addsuffix $(ch_semi),$(basename $(ir.wobj))))
 ir.wnml = $(subst $(ch_space),$(ch_comma),$(strip \
 	  $(subst wkt_,,$(basename $(ir.wobj)))))
 ir_readnml.f : ir_readnml.h
-	$(ccE) -DUSE_WKT='$(ir.wuse)' -DWKT_NML='$(ir.wnml)' $(FPPFLAGS) \
-	  $< | sed -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
+	$(ccE) -DUSE_WKT='$(ir.wuse)' -DWKT_NML='$(ir.wnml)' $(FPPFLAGS) $< | \
+	  sed -e 's:+/+/://:' -e 's/ *## *//' -e '/^#/d' -e '/^ *$$/d' > $@
 
 # Construct the IREP library.
 libIR.a : ir_readnml.o irep.o ir_std.o $(ir.wobj)
